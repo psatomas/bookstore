c9436bdf0b6372c6d77e60a0acaf3d41c4c4b9a8
psatomas <psatomas@gmail.com>
2025-12-24 16:22:46 -0300

    Refactor book_detail view to use get_object_or_404

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Book
 
@@ -11,7 +11,7 @@ def index(request):
     })
 
 def book_detail(request, id):
-   book = Book.objects.get(id=id)
+   book = get_object_or_404(Book, pk=id)
    return render(request, 'book_outlet/book_detail.html', {
       "title": book.title,
       "author": book.author,
